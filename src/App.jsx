e87df0f42f4f1d6e7ccf37fbb705a1baf735f80a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-03-21 20:35:58 +0800

    Add CITY component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -44,6 +45,8 @@ const App = () => {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
